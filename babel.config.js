5237b10054ca01abbcdd1ea7775d044f62784907
Ryan Liu <ryan@launchlab.com.au>
2019-10-31 15:07:16 +1100

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = api => {
+  const isTest = api.env('test')
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: ['last 2 versions', 'not dead', 'not < 2%', 'not ie 11'],
+          modules: isTest ? 'commonjs' : false,
+          useBuiltIns: 'entry',
+          corejs: '3'
+        }
+      ],
+      '@babel/preset-react'
+    ],
+    plugins: [
+      'react-hot-loader/babel',
+      '@babel/plugin-proposal-class-properties',
+      isTest ? 'dynamic-import-node' : '@babel/plugin-syntax-dynamic-import'
+    ]
+  }
+}
